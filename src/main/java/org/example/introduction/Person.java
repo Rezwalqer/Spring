63d512dc8c751f35d5ba736775c7f1d1242a71da
Ruslan <rezwalqer@gmail.com>
2023-02-25 21:51:12 +0500

    Spring hw2

--- src/main/java/org/example/introduction/Person.java
+++ src/main/java/org/example/introduction/Person.java
@@ -1,14 +1,26 @@
 package org.example.introduction;
 
-public class Person {
-    private Car car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-    public Person(Car car) {
+@Component
+public class Person {
+    private Tramsport car;
+
+    @Autowired
+    public Person(@Qualifier("moto") Tramsport car) {
+        this.car = car;
+    }
+
+
+    public void setCar(Tramsport car) {
+
         this.car = car;
     }
 
     public void drive() {
-        System.out.println("Владелец сел в автомобиль");
+        System.out.println("Владелец сел в транспорт");
         car.go();
     }
 }
